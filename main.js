2514b0ae553f5e590965291e38cdf1f76cd70ba3
Gabriel Miyazato <gabriel.2003mvf@gmail.com>
2021-08-09 23:43:57 -0300

    add index.html and main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+function snapCrackle (maxValue){
+    let str = ""
+   for(let i = 1; i <= maxValue; i++){
+     if(i%2 !== 2 && i%5 === 0){
+         str += " SnapCrackle,"
+     }
+     else if(i%2 !== 0){
+         str += " Snap,"
+     } 
+     else if(i%5 === 0){
+         str += " Crackle,"
+     }
+     else{
+         str += " " + i.toString() +","
+     }
+   }
+   return str
+}
